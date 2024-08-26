d6a746c45f594c67fe90de598b514378b6215806
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2024-08-26 20:29:21 +0800

    debug axios cors error #3

--- src/api/index.js
+++ src/api/index.js
@@ -2,15 +2,15 @@ import axios from 'axios'
 
 const JWToken = localStorage.token
 
-axios.interceptors.request.use(
-  config => {
-      config.headers.Authorization = `Bearer ${JWToken}`;
-      return config;
-  },
-  error => {
-      return Promise.reject(error);
-  }
-);
+// axios.interceptors.request.use(
+//   config => {
+//       config.headers.Authorization = `Bearer ${JWToken}`;
+//       return config;
+//   },
+//   error => {
+//       return Promise.reject(error);
+//   }
+// );
 
 export default(url = 'https://happy-radically-racer.ngrok-free.app/api') => {
   // export default(url = 'http://127.0.0.1:8000/api') => {
