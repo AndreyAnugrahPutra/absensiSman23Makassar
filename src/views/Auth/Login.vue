e203c7e14c4da08aed19d6018f1672777bf1eb4a
Andrey Anugrah Putra <andreyanugrahputra@gmail.com>
2024-08-31 20:29:01 +0800

    fix login #1

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -57,8 +57,6 @@ const login = async () =>
 
             const data = response.data.data
 
-            console.log(data)
-
             if(data.api_data)
             {     
                 localStorage.setItem('token', data.api_data.token)
